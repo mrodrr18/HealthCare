2902de6bf5e2aeff0ed2925c781fbb15f410965d
mrodrr18 <36730107+mrodrr18@users.noreply.github.com>
2021-05-17 18:01:26 +0200

    citas casi hecho falla el calendario

---
+++ src/main/java/com/unileon/controller/CitasController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unileon.controller;
+
+import com.unileon.modelo.Cita;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author mrrtr
+ */
+@Named
+@ViewScoped
+public class CitasController implements Serializable{
+    
+    private List <Cita> listaCitas;
+    private Cita nuevo; 
+    
+    @PostConstruct
+    public void init(){
+        nuevo = new Cita();
+        listaCitas = new ArrayList <Cita>();
+        
+        Cita c = new Cita();
+        c.setCausa("Dolor de cabeza");
+        c.setUrgente(0);
+        c.setFecha(new Date());
+        listaCitas.add(c);
+        
+        c = new Cita();
+        c.setCausa("Dolor de barriga");
+        c.setUrgente(1);
+        c.setFecha(new Date());
+        listaCitas.add(c);
+        
+    }
+    
+     public void guardarNuevacita(){
+        System.out.println("Nueva cita guardada");
+    }
+
+    public List<Cita> getListaCitas() {
+        return listaCitas;
+    }
+
+    public void setListaCitas(List<Cita> listaCitas) {
+        this.listaCitas = listaCitas;
+    }
+
+    public Cita getNuevo() {
+        return nuevo;
+    }
+
+    public void setNuevo(Cita nuevo) {
+        this.nuevo = nuevo;
+    }
+    
+}
